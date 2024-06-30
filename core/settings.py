3869dbec62ecf97a2871440c43076e1757cf9fd5
Preetam Raj <iitianpreetam@gmail.com>
2024-07-01 00:03:32 +0530

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -187,6 +187,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
+CORS_ALLOW_CREDENTIALS = True
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
